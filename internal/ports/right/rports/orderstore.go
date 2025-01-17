7d0310f28aa30947ace8d4dfbfee9350b402de14
Kshitiz Mhto <kshitizmhto101@gmail.com>
2025-01-18 05:33:30 +0545

    core: added oderstore endpoint and sql impl

--- internal/ports/right/rports/orderstore.go
+++ internal/ports/right/rports/orderstore.go
@@ -5,6 +5,14 @@ import (
 )
 
 type OrderStore interface {
-	CreateOrder(entity.Order) (int, error)
-	CreateOrderItem(entity.OrderItem) error
+	CreateOrder(order entity.Order) (string, error)       // Create a new order and return its ID
+	GetOrderByID(orderID string) (entity.Order, error)    // Retrieve an order by its ID
+	GetOrdersByUserID(userID int) ([]entity.Order, error) // Retrieve all orders for a specific user
+	UpdateOrder(order entity.Order) error                 // Update an existing order
+	DeleteOrder(orderID string) error                     // Delete an order and its associated items
+
+	CreateOrderItem(orderItem entity.OrderItem) error                  // Add an item to an order
+	GetOrderItemsByOrderId(orderID string) ([]entity.OrderItem, error) // Retrieve all items for a specific order
+	DeleteOrderItem(orderItemID string) error                          // Delete a specific order item
+
 }
